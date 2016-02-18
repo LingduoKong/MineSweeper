2d53b26a642aa623beb809629147d82bd3b4a664
Lingduo Kong <lingduokong@Lingduos-MacBook-Pro.local>
2016-02-18 14:21:50 -0800

    add test comments

--- Test/src/GamePanelTest.java
+++ Test/src/GamePanelTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
  */
 public class GamePanelTest {
 
+    // TODO: 2/18/16  for the current case, don't know how to write a unit test.
+    // TODO: Should it have a specific interface for a unit test or not?
     @Test
     public void tellDifference() {
 
--- test.java
+++ test.java
@@ -5,12 +5,12 @@ public class test {
 
     public static void main(String[] args) {
 
-        Game game = new Game(10, 10);
-//        game.start();
-        int count = 0;
-        for (int i = 0; i < 1000; i++) {
-            count += game.solver();
-        }
-        System.out.println(count);
+        Game game = new Game(10, 30);
+        game.start();
+//        int count = 0;
+//        for (int i = 0; i < 1000; i++) {
+//            count += game.solver();
+//        }
+//        System.out.println(count);
     }
 }
